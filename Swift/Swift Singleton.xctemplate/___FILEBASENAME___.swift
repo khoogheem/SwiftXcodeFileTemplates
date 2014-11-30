ec3621b7b35a6f670f1e3eb365891037182941bb
Kevin A. Hoogheem <kevin@hoogheem.net>
2014-11-30 14:47:08 -0600

    First commit of templates

---
+++ Swift/Swift Singleton.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+    
+    class var sharedInstance : ___FILEBASENAMEASIDENTIFIER___ {
+        struct Static {
+            static var onceToken : dispatch_once_t = 0
+            static var instance : ___FILEBASENAMEASIDENTIFIER___? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = ___FILEBASENAMEASIDENTIFIER___()
+        }
+        return Static.instance!
+    }
+}
