e52d66f9b5aefd50f1ea17d2f69e24154d40637a
Kevin A. Hoogheem <kevinh@npe-inc.com>
2015-02-05 14:47:30 -0600

    fixed up comments.. added uiviewcontroller

---
+++ Swift/Swift UIViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,39 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+#if os(iOS)
+    import UIKit
+#elseif os(OSX)
+    import Cocoa
+#endif
+
+
+/** ___FILEBASENAMEASIDENTIFIER___ Class
+
+*/
+class ___FILEBASENAMEASIDENTIFIER___ : UITableViewController {
+
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+}
