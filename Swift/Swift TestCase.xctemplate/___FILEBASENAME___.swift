57d7ac0577af5ef538cb6cad6952f5aa3c854de0
dmiedema <dmiedema@arubanetworks.com>
2015-04-14 16:25:47 -0700

    Add Swift XCTestCase subclass template

---
+++ Swift/Swift TestCase.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,39 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+import XCTest
+
+/** ___FILEBASENAMEASIDENTIFIER___ Class
+
+*/
+class ___FILEBASENAMEASIDENTIFIER___ : XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssertTrue(true, "Pass")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    // MARK: - Setup
+    override func setUp() {
+        super.setUp()
+        // Put Setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
